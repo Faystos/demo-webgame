c600a35013806270079507780d37c7a0953d8e82
vsadovnikov <vsadovnikov@ptsecurity.com>
2023-12-18 21:24:58 +0300

    create starter pack angular and phaser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AUTO, Game, Scene, Types } from 'phaser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular-And-Phaser-Starter-Pack';
+export class AppComponent extends Scene{
+  phaserGame!: Phaser.Game;
+  config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    scene: [this]
+  };
+
+  constructor() {
+    super('Game');
+    this.phaserGame = new Game(this.config);
+  }
+
+  preload() {}
+
+  create(){}
+
+  override update() {}
+
 }
